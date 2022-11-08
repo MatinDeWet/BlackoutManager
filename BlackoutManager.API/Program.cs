6c50ba100b0ac02064a8441147245e6e3d9c221d
Matin de Wet <Matin@work.za.com>
2022-11-08 15:13:09 +0200

    addition of authentication and identity

--- BlackoutManager.API/Program.cs
+++ BlackoutManager.API/Program.cs
@@ -9,6 +9,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Host.UseSerilog((context, loggerConfiruration) => loggerConfiruration.WriteTo.Console().ReadFrom.Configuration(context.Configuration));
 
 builder.Services.AddApplicationPostGresContext(builder.Configuration);
+builder.Services.AddApplicationAuthentication(builder.Configuration);
+builder.Services.AddApplicationIdentity(builder.Configuration);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
--- BlackoutManager.CORE/Extensions/ApplicationServiceExtensions.cs
+++ BlackoutManager.CORE/Extensions/ApplicationServiceExtensions.cs
@@ -1,7 +1,12 @@
 ﻿using BlackoutManager.DATA.EF;
+using BlackoutManager.DATA.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace BlackoutManager.CORE.Extensions;
 
@@ -13,6 +18,40 @@ public static class ApplicationServiceExtensions
         {
             options.UseNpgsql(_config.GetConnectionString("PostGresConnectionString"));
         });
+
+        return services;
+    }
+
+    public static IServiceCollection AddApplicationAuthentication(this IServiceCollection services, IConfiguration _config)
+    {
+        services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ClockSkew = TimeSpan.Zero,
+                ValidIssuer = _config["JWTSettings:Issuer"],
+                ValidAudience = _config["JWTSettings:Audience"],
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["JWTSettings:Key"]))
+            };
+        });
+
+        return services;
+    }
+    public static IServiceCollection AddApplicationIdentity(this IServiceCollection services, IConfiguration _config)
+    {
+        services.AddIdentityCore<User>()
+            .AddRoles<IdentityRole>()
+            .AddTokenProvider<DataProtectorTokenProvider<User>>(_config["JWTSettings:Issuer"])
+            .AddEntityFrameworkStores<PostGresContext>()
+            .AddDefaultTokenProviders();
 
         return services;
     }
